ea2b16ab938d967b311a0784289099c73127f940
ducthinhhh <92781079+ducthinhhh@users.noreply.github.com>
2021-10-19 14:49:04 +0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,13 @@
+<!-- //cuối trnag -->
+	<!-- footer -->
+
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 302  | Design by 302Team
+				
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
